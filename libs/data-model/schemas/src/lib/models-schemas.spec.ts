962569301d051e615303bee8ef8c8e51ec00e3b9
GuiGaIb <gui.gaib.mex@gmail.com>
2026-04-06 04:46:48 -0600

    feat(models-schemas): init data model schemas lib

---
+++ libs/data-model/schemas/src/lib/models-schemas.spec.ts
@@ -0,0 +1,7 @@
+import { modelsSchemas } from './models-schemas';
+
+describe('modelsSchemas', () => {
+  it('should work', () => {
+    expect(modelsSchemas()).toEqual('models-schemas');
+  });
+});
---
+++ libs/data-model/schemas/src/lib/models-schemas.ts
@@ -0,0 +1,3 @@
+export function modelsSchemas(): string {
+  return 'models-schemas';
+}
