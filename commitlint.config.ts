794614794053d075e84c3b069d4d6afe5bfbdd2b
GuiGaIb <gui.gaib.mex@gmail.com>
2026-04-06 02:16:45 -0600

    chore(root): workspace git hooks and tools
    Add Husky, lint-staged, Prettier, commitlint, and docs/GIT.md.

---
+++ commitlint.config.ts
@@ -0,0 +1,35 @@
+import { RuleConfigSeverity, type UserConfig } from '@commitlint/types';
+
+const Configuration: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-case': [
+      RuleConfigSeverity.Error,
+      'always',
+      {
+        cases: ['kebab-case'],
+        delimiters: ['/'],
+      },
+    ],
+    'scope-empty': [RuleConfigSeverity.Error, 'never'],
+    'header-max-length': [RuleConfigSeverity.Error, 'always', 50],
+    'type-enum': [
+      RuleConfigSeverity.Error,
+      'always',
+      [
+        'chore', // Chores, non-functional changes
+        'feat', // Features
+        'fix', // Bug fixes
+        'docs', // Documentation
+        'style', // Code style, formatting, etc.
+        'refactor', // Code refactoring, renaming, etc.
+        'perf', // Performance improvements
+        'test', // Testing, adding tests, refactoring tests, etc.
+        'build', // Build system
+        'ci', // CI/CD
+      ],
+    ],
+  },
+};
+
+export default Configuration;
