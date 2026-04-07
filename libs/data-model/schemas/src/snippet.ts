4f0540ceba5d31b790390b653ebf36f5819287fd
GuiGaIb <gui.gaib.mex@gmail.com>
2026-04-07 02:38:42 -0600

    feat(models-schemas): add snippet schema

---
+++ libs/data-model/schemas/src/snippet.ts
@@ -0,0 +1,45 @@
+import type { TSnippet, TSnippetDependency } from '@models/types/snippet';
+import z from 'zod';
+import { getNormalizedLineSchema } from './shared/normalizedLine.js';
+import { getNormalizedMultilineSchema } from './shared/normalizedMultiline.js';
+import { objectIdLikeSchema } from './shared/objectIdLike.js';
+
+export const snippetDependencySchema = z.object({
+  name: getNormalizedLineSchema({
+    minLength: 1,
+    maxLength: 256,
+  }),
+  version: getNormalizedLineSchema({
+    minLength: 1,
+    maxLength: 256,
+  }).optional(),
+}) satisfies z.ZodType<TSnippetDependency>;
+
+export const snippetSchema = z.object({
+  id: objectIdLikeSchema,
+  title: getNormalizedLineSchema({
+    minLength: 1,
+    maxLength: 128,
+  }),
+  description: getNormalizedMultilineSchema({
+    minLength: 1,
+    maxLength: 1024,
+  }).optional(),
+  language: objectIdLikeSchema,
+  code: getNormalizedMultilineSchema({
+    minLength: 1,
+    maxLength: 65536,
+  }),
+  dependencies: z.array(snippetDependencySchema),
+  tags: z
+    .array(
+      getNormalizedLineSchema({
+        minLength: 1,
+        maxLength: 64,
+      }),
+    )
+    .transform((tags) =>
+      // Lowercase and deduplicate
+      Array.from(new Set([...tags.map((tag) => tag.toLowerCase())])),
+    ),
+}) satisfies z.ZodType<TSnippet>;
